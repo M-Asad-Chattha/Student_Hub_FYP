086297f70fa1a93df32b5a4b1ce78009260e9348
AsadChattha <m.asad.chatthaa@gmail.com>
2019-05-16 22:47:44 +0500

    Profile UI Almost Done

--- app/src/main/java/com/achba/studenthub/ProfileActivity.java
+++ app/src/main/java/com/achba/studenthub/ProfileActivity.java
@@ -1,8 +1,11 @@
 package com.achba.studenthub;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -21,5 +24,26 @@ public class ProfileActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    public void onClickEditProfile(View view) {
+        Toast.makeText(this, "Edit Profile", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickDeleteAccount(View view) {
+        Toast.makeText(this, "Delete account", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickSeeMore(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(ProfileActivity.this);
+        builder.setMessage("User hasn't added more About Info.")
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //do things
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
